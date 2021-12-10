db5b937ee602c0fd34c8c09c02f20eae82fac548
Gabriel Torres <al2012.163.torres@gmail.com>
2021-12-10 01:01:34 -0300

    Project start

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', async (req, res) => res.sendStatus(200));
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import './setup';
+import app from './app';
+
+app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}.`));
